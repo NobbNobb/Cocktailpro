47c6a53561476b055a939b5bdc3e95c2d4e45930
fs <fs@fs-VirtualBox>
2015-12-12 22:39:41 +0100

    Kommentare und Formatierung

--- Cocktailpro/Rezeptur_Prozessor.cpp
+++ Cocktailpro/Rezeptur_Prozessor.cpp
@@ -5,19 +5,16 @@
 
 #include "Rezeptur_Prozessor.h"
 #include "Zeit.h"
-#include <iostream>
-
-Rezeptur_Prozessor::Rezeptur_Prozessor(){
-    
-}
 
 Rezeptur_Prozessor::Rezeptur_Prozessor(Dosierer_Verwaltung *dVerwaltung){
+    //Erstellen aller benötigten Objekte
     Zeit* zeit = new Zeit();
     Stampfer* stampfer = new Stampfer(zeit);
     Mischer* mischer = new Mischer(zeit);
     Waage* waage = new Waage(zeit);
     Entleerer* entleerer = new Entleerer(waage, zeit);
     
+    //Speichern aller benötigten Objekte
     m_Zeit = zeit;
     m_Dosierer_Verw = dVerwaltung;
     m_Stampfer = stampfer;
@@ -25,40 +22,42 @@ Rezeptur_Prozessor::Rezeptur_Prozessor(Dosierer_Verwaltung *dVerwaltung){
     m_Waage = waage;
     m_Entleerer = entleerer;
     
+    //Hinzufügen des Waagen-Objektes zu jedem Dosierer-Objekt
     for(int i = 0; i < m_Dosierer_Verw->getM_Dosierer().size(); i++){
         m_Dosierer_Verw->getM_Dosierer().at(i)->setSubject(m_Waage);
     }
     
 }
 
-Rezeptur_Prozessor::~Rezeptur_Prozessor(){
-    
+Rezeptur_Prozessor::~Rezeptur_Prozessor(){   
 }
-/**
- * 
- * */
-void Rezeptur_Prozessor::bereite_zu(Rezept* rezept)
-{
-    for(int i = 0; i < rezept->getAnzahlRezeptschritte(); i++){
-        Rezeptschritt* rSchritt = rezept->getRezeptSchritt(i);
-        if(rSchritt->getZutat() == "Mischen"){
-            m_Mischer->mischen(rSchritt->getMenge());
+
+/*-----------------------Getter-----------------------*/
+
+Zeit* Rezeptur_Prozessor::getZeit() const{
+    return m_Zeit;
+}
+
+/*-----------------------Funktionen-----------------------*/
+
+void Rezeptur_Prozessor::bereite_zu(Rezept* rezept){
+    for(int i = 0; i < rezept->getAnzahlRezeptschritte(); i++){    //Alle Rezeptschritte durchlaufen         
+        Rezeptschritt* rSchritt = rezept->getRezeptSchritt(i);     //Rezeptschritt-Objekt erstellen und Rezeptschritt speichern
+        if(rSchritt->getZutat() == "Mischen"){                     //Prüfen ob die Zutat "Mischen" ist
+            m_Mischer->mischen(rSchritt->getMenge());              //Cocktail mischen
         }
-        else if(rSchritt->getZutat() == "Stampfen"){
-            m_Stampfer->stampfen(rSchritt->getMenge());
+        else if(rSchritt->getZutat() == "Stampfen"){               //Prüfen ob die Zutat "Stampfen" ist
+            m_Stampfer->stampfen(rSchritt->getMenge());            //Cocktail stampfen
         }
         else{
-            for(int i = 0; i < m_Dosierer_Verw->getM_Dosierer().size(); i++){
-                if(m_Dosierer_Verw->getM_Dosierer().at(i)->getZutat() == rSchritt->getZutat()){
-                    m_Dosierer_Verw->getM_Dosierer().at(i)->dosieren(rSchritt->getMenge());
+            for(int i = 0; i < m_Dosierer_Verw->getM_Dosierer().size(); i++){   //Alle Dosierer durchlaufen
+                if(m_Dosierer_Verw->getM_Dosierer().at(i)->getZutat() == rSchritt->getZutat()){ //Dosierer-Zutat mit Rezeptschritt-Zutat vergleichen
+                    m_Dosierer_Verw->getM_Dosierer().at(i)->dosieren(rSchritt->getMenge());     //Zutat dosieren
                 }
             }
         }
     }
-    m_Entleerer->leeren();
-    m_Entleerer->reinigen();
+    m_Entleerer->leeren();              //Behälter leeren
+    m_Entleerer->reinigen();            //Behälter reinigen
 }
 
-Zeit* Rezeptur_Prozessor::getTimer() const{
-    return m_Zeit;
-}
--- Cocktailpro/Rezeptur_Prozessor.h
+++ Cocktailpro/Rezeptur_Prozessor.h
@@ -19,45 +19,65 @@
 class Rezeptur_Prozessor
 {
 	
-public:
-        Rezeptur_Prozessor();
+    public:
+        /**
+         * Konstruktor zum erstellen eines Rezepturprozessors
+         * @param dVerwaltung Pointer auf ein Dosierer_Verwaltung-Objekt
+         */
         Rezeptur_Prozessor(Dosierer_Verwaltung *dVerwaltung);
+        
+        /**
+         * Standarddestruktor
+         */
         virtual ~Rezeptur_Prozessor();
-	/**
-	 * 
-	 * */
-	void bereite_zu(Rezept* rezept);
-        Zeit* getTimer() const;
 	
-private:
+        /*-----------------------Getter-----------------------*/
+        
+        /**
+         * gibt das Zeit-Objekt zurück
+         * @return Zeit-Objekt
+         */
+        
+        Zeit* getZeit() const;
+        
+        /*-----------------------Funktionen-----------------------*/
+        
+        /**
+         * Koordiniert die Hardware anhand des zu mischenden Rezeptes
+         * @param rezept Das Rezept das gemischt werden sool
+         */
+	void bereite_zu(Rezept* rezept);
+	
+    private:
 	/**
-	 * 
+	 * Das Objekt der Dosiererverwaltung
 	 * */
 	Dosierer_Verwaltung * m_Dosierer_Verw;
 	
 	/**
-	 * 
+	 * Das Objekt des Entleerers
 	 * */
 	Entleerer * m_Entleerer;
 	
 	/**
-	 * 
+	 * Das Objekt des Mischers
 	 * */
 	Mischer * m_Mischer;
 	
 	/**
-	 * 
+	 * Das Objekt des Stampfers
 	 * */
 	Stampfer * m_Stampfer;
 	
 	/**
-	 * 
+	 * Das Objekt der Waage
 	 * */
 	Waage * m_Waage;
         
+        /**
+         * Das Zeit-Objekt zum warten
+         */
         Zeit*  m_Zeit;
-	
-	
 };
 
 #endif
