1004f89fc4af06e6265f3c2dedc990f903374b93
fs <fs@fs-VirtualBox>
2015-12-12 22:07:23 +0100

    Kommentare und Formatierung

--- Cocktailpro/Observer.h
+++ Cocktailpro/Observer.h
@@ -6,17 +6,16 @@
 #define OBSERVER_H_H
 
 /**
- * 
+ * Abstrakte Observer-Klasse. Funktionen in den konkreten Observern implementiert
  */
 class Observer
 {
 	
 public:
 	/**
-	 * 
+	 * Funktion zur Aktuallisierung nach Änderung beim zu beobachtenden Objektes (virtual)
 	 */
-	virtual void update() = 0;
-	
+	virtual void update() = 0;	
 };
 
 #endif
